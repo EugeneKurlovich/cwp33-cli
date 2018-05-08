d9744aed3c617d65b4fd2ae565f300070d1e45ae
EugeneKurlovich <zhenikpggkurlovich@gmail.com>
2018-05-08 08:37:07 +0300

    db

--- data/index.js
+++ data/index.js
@@ -2,11 +2,15 @@ const Sequelize = require('sequelize');
 const config = require('../config.json');
 
 const options = {
-    host: config.db.host,
-    dialect: 'mysql',
-    logging: false
+    host: "ec2-50-19-232-205.compute-1.amazonaws.com",
+    dialect: 'postgres',
+    port:5432,
+    logging: false,
+    dialectOptions:{
+        ssl : true
+    }    
 };
-const sequelize = new Sequelize(config.db.name, config.db.user, config.db.password, options);
+const sequelize = new Sequelize("davtq49h0m1cr5", "avjjrhufewzvgx", "5ee36876842730e36bbb71a8881ad310594b09d78387b35ce1948367ff82cabe", options);
 
 const Repo = require('./repo')(Sequelize, sequelize);
 const Commit = require('./commit')(Sequelize, sequelize);
