1d7231b4f4e5f401c5a4d0a52515cf43f21aba15
EugeneKurlovich <zhenikpggkurlovich@gmail.com>
2018-05-08 08:57:19 +0300

    sfsds

--- index.js
+++ index.js
@@ -1,57 +1,11 @@
-const http = require('http');
-const express = require('express');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const { Ability, AbilityBuilder, ForbiddenError } = require('casl');
+const Sequelize = require("sequelize");
+const config = require("./config.json");
+const dbs = require("./cont/context.js");
+const db = dbs(Sequelize, config);
 
-const context = require('./data');
-const insertData = require('./data/insert-data');
-const apiRouter = require('./routes');
+const server = require("./server")(db);
+const port = process.env.PORT || 7000;
 
-insertData(context);
-
-const app = express(http);
-
-
-app.use(morgan('dev'));
-
-app.use(bodyParser.json({ type: 'application/json' }));
-
-app.use('/api', (req, res, next) => {
-    const { rules, can, cannot } = AbilityBuilder.extract();
-    const role = req.query.role || 'guest';
-
-    if (role === 'guest') {
-        can('read', 'all');
-    }
-
-    if (role === 'member') {
-        can('read', 'all');
-        can('create', 'Repo');
-        can('update', 'Repo', { author: req.query.author });
-        can(['create', 'update'], 'Commit');
-    }
-
-    if (role === 'moderator') {
-        can('read', 'all');
-        can('update', ['Repo', 'Commit']);
-        can('delete', ['Repo', 'Commit']);
-        can('create', ['Repo', 'Commit']);
-    }
-
-    req.ability = new Ability(rules);
-
-    next();
+db.sequelize.sync().then(() => {
+  server.listen(port, () => console.log("Running on port " + port));
 });
-
-app.use('/api/v1/', apiRouter);
-
-app.use((error, req, res, next) => {
-    if (error instanceof ForbiddenError) {
-        res.status(403).send({ message: error.message })
-    } else {
-        res.send(error);
-    }
-});
-
-app.listen(3000);
