a469e1a23a5c9a873d996c72b1a70106071d0028
EugeneKurlovich <zhenikpggkurlovich@gmail.com>
2018-05-08 09:28:12 +0300

    asda

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const Sequelize = require("sequelize");
-//const config = require("./config.json");
-//const dbs = require("./cont/context.js");
-//const db = dbs(Sequelize, config);
+const config = require("./config.json");
+const dbs = require("./cont/context.js");
+const db = dbs(Sequelize, config);
 
 const server = require("./server")();
 const port = process.env.PORT || 7000;
-server.listen(port, () => console.log("Running on port " + port));
 
-/*db.sequelize.sync().then(() => {
+db.sequelize.sync().then(() => {
+  server.listen(port, () => console.log("Running on port " + port));
 });
-*/
--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-//const abilitiesChecker = require("./repositories/abilities");
+const abilitiesChecker = require("./repositories/abilities");
 module.exports = () => {
-  /*console.log(db.Repos)
-  console.log(db.Commits)
   const ReposRepository = require("./repositories/reposrepository")(db.Repos);
   const CommitsRepository = require("./repositories/CommitsRepository")(
     db.Commits
@@ -11,12 +9,12 @@ module.exports = () => {
   const api = require("./controllers/apicontroller")(
     CommitsRepository,
     ReposRepository
-  );*/
+  );
   const app = express();
-  //app.get("/api/abilities", abilitiesChecker.rules);
-  //app.use("/*", abilitiesChecker.checkAbilities);
+  app.get("/api/abilities", abilitiesChecker.rules);
+  app.use("/*", abilitiesChecker.checkAbilities);
   app.use(bodyParser.json());
-  //app.use("/api", api);
+  app.use("/api", api);
   app.use("/", function(req, res) {
     res.send("testing page 322");
   });
